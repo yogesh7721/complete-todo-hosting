1083846ebc8977d5c2082dbf5432e293b750ed60
yogesh shelke <shelkeyogesh874@gmail.com>
2024-11-19 08:52:56 +0530

    dist adde

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(express.static("dist"))
+
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
